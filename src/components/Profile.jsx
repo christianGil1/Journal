2d45318d55cf09ca08c730172accb5de96ddf7b2
Christian Gil Alaan <christiangilalaan@gmail.com>
2024-12-03 21:29:43 +0800

    fixed typo yet again

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -135,7 +135,7 @@ export const Profile = () => {
                         </p>
                     </div>
                     <div className="bg-white p-6 shadow-lg rounded-lg flex flex-col items-center hover:shadow-xl transition-all">
-                        <h2 className="mb-4 font-semibold text-gray-800">[ May 2023 - Aug 2023 ]</h2>
+                        <h2 className="mb-4 font-semibold text-gray-800">[ May 2023 - August 2023 ]</h2>
                         <img src="https://i.ibb.co/nLVBLZ2/5.jpg" alt="Documentation 2" className="rounded mb-4" />
                         <h3 className="text-lg font-semibold text-gray-800">Student Assistant at Xavier University</h3>
                         <p className="text-gray-600 mt-2 mb-2">
